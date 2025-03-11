ab9cef0d53c3b46ddfe189935569c9d517c2e8b5
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 19:14:57 +0800

    Update: count_odd_numbers.py
    Initialize the count of odd numbers to 0

--- count_odd_numbers.py
+++ count_odd_numbers.py
@@ -1,6 +1,9 @@
 # Program: Count the Odd Numbers in 10 Inputs
-# This program asks the user to input 10 numbers and counts how many of them are odd.
+# This program asks the user to input 10 numbers and counts how many of them are odd
+
 # Initialize the count of odd numbers to 0
+odd_count = 0
+
 # Loop through 10 times to get the numbers from the user
 # Check if the number is odd
 # Print the total count of odd numbers
