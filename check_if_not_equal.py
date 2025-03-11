7bbecab664bdf168f7d227c2436ad8fac8d9711d
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 22:55:34 +0800

    Added: check_if_not_equal.py
    pseudocode Program: Check if the Numbers are Not Equal

---
+++ check_if_not_equal.py
@@ -0,0 +1,5 @@
+# Program: Check if the Numbers are Not Equal
+# This program asks the user to input two numbers and prints "Not Equal" 
+# if the numbers are different
+# Ask the user for two numbers
+# Check if the numbers are not equal and print "Not Equal" if true
