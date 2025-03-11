edac3e7701081123d9653c7dc4560fd670c413a6
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 23:20:49 +0800

    Updated: integer_quotient_of_two_numbers.py
    pseudocode Program: Print the Remainder of Two Numbers

---
+++ remainder_of_two_numbers.py
@@ -0,0 +1,4 @@
+# Program: Print the Remainder of Two Numbers
+# This program asks the user to input two numbers and prints the remainder when the first number is divided by the second
+# Ask the user for two numbers
+# Calculate the remainder and print the result
