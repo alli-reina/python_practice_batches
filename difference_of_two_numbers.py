e370ec3b5b39a0c6293d4e7d42b83d9ec8d4f164
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 23:07:55 +0800

    Added: difference_of_two_numbers.py
    Pseudocode Program: Print the Difference of Two Numbers

---
+++ difference_of_two_numbers.py
@@ -0,0 +1,4 @@
+# Program: Print the Difference of Two Numbers
+# This program asks the user to input two numbers and prints the difference between them
+# Ask the user for two numbers
+# Calculate the difference and print the result
