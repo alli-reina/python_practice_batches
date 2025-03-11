c4098076dd70b9f129754970cc723c05aa5a9836
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 18:03:45 +0800

    Added product_of_two_numbers.py
    Pseudocode Program: Print the Product of Two Numbers

---
+++ product_of_two_numbers.py
@@ -0,0 +1,4 @@
+# Program: Print the Product of Two Numbers
+# This program asks the user for two numbers and prints their product.
+# Ask the user for two numbers
+# Calculate the product of the two numbers and print the result
