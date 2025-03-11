878783fd195d298af43835b9c922a24ee6729aa2
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 19:00:19 +0800

    Updated raise_first_number_to_second.py
    Ask the user for two numbers

--- raise_first_number_to_second.py
+++ raise_first_number_to_second.py
@@ -1,6 +1,10 @@
 # Program: Raise the First Number to the Power of the Second Number
-# This program asks the user for two numbers and prints the result when the first number is raised to the power of the second number.
+# This program asks the user for two numbers and prints the result when the first number is raised to the power of the second number
+
 # Ask the user for two numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
 # Calculate num1 raised to the power of num2
 # Print the result
 
