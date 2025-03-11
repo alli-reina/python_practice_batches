3d8558a1ab0dab5b7e30142d0c3b6864b92dc2e4
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 19:25:17 +0800

    Updated: print_numbers_excluding_ends_with_zero.py
    Loop through numbers from 0 to 100

--- print_numbers_excluding_ends_with_zero.py
+++ print_numbers_excluding_ends_with_zero.py
@@ -1,4 +1,7 @@
 # Program: Print Numbers from 0 to 100, Excluding Numbers Ending in Zero
 # This program prints all numbers from 0 to 100 except for those ending in zero
+
 # Loop through numbers from 0 to 100
+for i in range(0, 101):
+    
 # Print the number if it doesn't end in zero
