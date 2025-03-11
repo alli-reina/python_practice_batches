c12a59fb65ac9ee3ee184d04e498785ffa8f65c3
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 19:09:53 +0800

    Updated: sum_of_10_numbers.py
    Loop through 10 times to get the numbers from the user

--- sum_of_10_numbers.py
+++ sum_of_10_numbers.py
@@ -5,4 +5,8 @@
 total_sum = 0
 
 # Loop through 10 times to get the numbers from the user
+for i in range(10):
+    num = int(input(f"Enter number {i+1}: "))
+    total_sum += num
+    
 # Print the total sum
