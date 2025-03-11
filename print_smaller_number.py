d21478cf64fcfc41cd0c6e764ca55002e5e1acb8
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 22:47:05 +0800

    Added: print_smaller_number.py
    Pseudocode Program: Print the Smaller Number

---
+++ print_smaller_number.py
@@ -0,0 +1,5 @@
+# Program: Print the Smaller Number
+# This program asks the user to input two numbers and prints the smaller one.
+# If the numbers are equal, it prints "Equal".
+# Ask the user for two numbers
+# Compare the two numbers and print the smaller one or if they are equal
