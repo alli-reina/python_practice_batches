f0af51671d0f8a30922758e8b2aca51e6e6accf2
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 18:43:36 +0800

    Added quotient_of_two_numbers.py
    Program: Print the Quotient of Two Numbers

---
+++ quotient_of_two_numbers.py
@@ -0,0 +1,5 @@
+# Program: Print the Quotient of Two Numbers
+# This program asks the user for two numbers and prints the quotient of the division (with decimal).
+# If the second number is zero, it prints an error message.
+# Ask the user for two numbers
+# Check if the second number is zero to avoid division by zero error
