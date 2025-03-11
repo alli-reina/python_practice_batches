0e34e67978149642aab0b0ddfca07dda78f9d0af
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 18:07:01 +0800

    Updated product_of_two_numbers.py
    Ask the user for two numbers

--- product_of_two_numbers.py
+++ product_of_two_numbers.py
@@ -1,4 +1,8 @@
 # Program: Print the Product of Two Numbers
 # This program asks the user for two numbers and prints their product.
+
 # Ask the user for two numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
 # Calculate the product of the two numbers and print the result
