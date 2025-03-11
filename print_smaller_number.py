99f70a5e8480eab8f825f1e1b19e511ef31bf86e
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 22:51:50 +0800

    Updated: print_smaller_number.py
    Compare the two numbers and print the smaller one or if they are equal

--- print_smaller_number.py
+++ print_smaller_number.py
@@ -7,3 +7,9 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
 # Compare the two numbers and print the smaller one or if they are equal
+if num1 < num2:
+    print("Smaller number:", num1)
+elif num1 > num2:
+    print("Smaller number:", num2)
+else:
+    print("The numbers are equal.")
