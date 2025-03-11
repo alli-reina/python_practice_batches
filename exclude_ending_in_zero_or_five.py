4af8a4aa0d5500cbb6355e776e93897c03f8c6aa
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-12 00:09:47 +0800

    Added: exclude_ending_in_zero_or_five.py
    pseudocode Program: Print Numbers from 0 to 100, excluding those ending in 0 or 5

---
+++ exclude_ending_in_zero_or_five.py
@@ -0,0 +1,3 @@
+# Program: Print Numbers from 0 to 100, excluding those ending in 0 or 5
+# This program prints all the numbers from 0 to 100, but skips the ones ending in 0 or 5
+# Loop through the numbers from 0 to 100
