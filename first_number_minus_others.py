726a4ad4b0a696173111a1f7c7fc063f571dbf3d
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 23:34:01 +0800

    Updated: first_number_minus_others.py
    Ask the user for the first number

--- first_number_minus_others.py
+++ first_number_minus_others.py
@@ -1,6 +1,9 @@
 # Program: Subtract the Remaining Numbers from the First Number
 # This program asks the user to input 10 numbers and prints the result of the first number minus all of the remaining numbers
+
 # Ask the user for the first number
+num1 = int(input("Enter the first number: "))
+
 # Loop to subtract all remaining numbers from num1
 # Print the final result
 
