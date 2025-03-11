b637bf45d29eed132f1f49ab079cb96bbf0c3c49
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 22:49:47 +0800

    Updated: print_smaller_number.py
    Ask the user for two numbers

--- print_smaller_number.py
+++ print_smaller_number.py
@@ -1,5 +1,9 @@
 # Program: Print the Smaller Number
-# This program asks the user to input two numbers and prints the smaller one.
-# If the numbers are equal, it prints "Equal".
+# This program asks the user to input two numbers and prints the smaller one
+# If the numbers are equal, it prints "Equal"
+
 # Ask the user for two numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
 # Compare the two numbers and print the smaller one or if they are equal
