df99699b71d32427423a68c08288cdc6abf49b24
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 19:08:21 +0800

    Updated:  sum_of_10_numbers.py
    Initialize the total sum to 0

--- sum_of_10_numbers.py
+++ sum_of_10_numbers.py
@@ -1,5 +1,8 @@
 # Program: Calculate the Sum of 10 Numbers
-# This program asks the user to input 10 numbers and prints the sum of those numbers.
+# This program asks the user to input 10 numbers and prints the sum of those numbers
+
 # Initialize the total sum to 0
+total_sum = 0
+
 # Loop through 10 times to get the numbers from the user
 # Print the total sum
