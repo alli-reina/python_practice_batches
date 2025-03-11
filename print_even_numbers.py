45eb5d97a6f5f3395ead284aaa7562157d8c5d22
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 19:22:14 +0800

    Updated: print_even_numbers.py
    Loop through numbers from 0 to 100 with a step of 2

--- print_even_numbers.py
+++ print_even_numbers.py
@@ -1,3 +1,6 @@
 # Program: Print Even Numbers from 0 to 100
 # This program prints all even numbers from 0 to 100 using a for loop
+
 # Loop through numbers from 0 to 100 with a step of 2
+for i in range(0, 101, 2):
+    print(i)
