33699cf9e4f658de75f984322327babce911fffa
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 23:44:22 +0800

    Updated: count_even_numbers.py
    Loop to ask the user to input 10 numbers

--- count_even_numbers.py
+++ count_even_numbers.py
@@ -5,5 +5,8 @@
 even_count = 0
 
 # Ask the user to input 10 numbers
+for i in range(10):  # Loop to input 10 numbers
+    num = int(input(f"Enter number {i + 1}: "))
+    
 # Check if the number is even
 # Print the total number of even numbers
