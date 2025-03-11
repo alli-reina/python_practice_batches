5f462216d4ceb84faa922ccb7b37f97fd93070e4
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-12 00:07:54 +0800

    Updated: numbers_between_two_numbers.py
    Print the numbers between num1 and num2

--- numbers_between_two_numbers.py
+++ numbers_between_two_numbers.py
@@ -6,3 +6,11 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
 # Print the numbers between num1 and num2
+if num1 < num2:
+    for i in range(num1 + 1, num2):
+        print(i, end=" ")
+elif num1 > num2:
+    for i in range(num2 + 1, num1):
+        print(i, end=" ")
+else:
+    print("The numbers are the same, no numbers in between.")
