d877b16012182d70f4ca1d1ba2f2f4d3350a896b
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 23:32:54 +0800

    Added: first_number_minus_others.py
    pseudo code Program: Subtract the Remaining Numbers from the First Number

---
+++ first_number_minus_others.py
@@ -0,0 +1,6 @@
+# Program: Subtract the Remaining Numbers from the First Number
+# This program asks the user to input 10 numbers and prints the result of the first number minus all of the remaining numbers
+# Ask the user for the first number
+# Loop to subtract all remaining numbers from num1
+# Print the final result
+
