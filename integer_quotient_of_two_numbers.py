4fdf2d9e9db72f057f7ba04d32a13095f620dfbe
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 23:16:09 +0800

    Added: integer_quotient_of_two_numbers.py
    pseudocode Program: Print the Quotient of Two Numbers

---
+++ integer_quotient_of_two_numbers.py
@@ -0,0 +1,4 @@
+# Program: Print the Quotient of Two Numbers
+# This program asks the user to input two numbers and prints the quotient (integer division) of the two numbers
+# Ask the user for two numbers
+# Calculate the quotient and print the result
