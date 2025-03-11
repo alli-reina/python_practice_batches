d6d54e9cff5ab8278c2888e40fa5e7feaa815220
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 19:24:12 +0800

    Added: print_numbers_excluding_ends_with_zero.py
    Pseudocode Program: Print Numbers from 0 to 100, Excluding Numbers Ending in Zero

---
+++ print_numbers_excluding_ends_with_zero.py
@@ -0,0 +1,4 @@
+# Program: Print Numbers from 0 to 100, Excluding Numbers Ending in Zero
+# This program prints all numbers from 0 to 100 except for those ending in zero
+# Loop through numbers from 0 to 100
+# Print the number if it doesn't end in zero
