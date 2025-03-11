f40fcb7d1be743c25eac93ef953aa5359c06cda8
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 17:38:12 +0800

    Updated check_equal_numbers.py
    Aks for the user's 1st and 2nd numbers

--- check_equal_numbers.py
+++ check_equal_numbers.py
@@ -1,5 +1,9 @@
 # Program Name: Check if Two Numbers Are Equal
-# This program asks the user to input two numbers and checks if they are equal.
-# If the numbers are equal, it prints "Equal", otherwise it prints "Not Equal".
+# This program asks the user to input two numbers and checks if they are equal
+# If the numbers are equal, it prints "Equal", otherwise it prints "Not Equal"
+
 # Ask the user for two numbers
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+
 # Compare the two numbers and print the result
