240da378b9e0ee643055fabd7ef2cb31bfda6312
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 23:59:53 +0800

    Added: numbers_between_two_numbers.py
    pseudocode Program: Print All Numbers Between Two Numbers

---
+++ numbers_between_two_numbers.py
@@ -0,0 +1,4 @@
+# Program: Print All Numbers Between Two Numbers
+# This program asks the user to input two numbers and prints all the numbers between them (exclusive)
+# Ask the user for two numbers
+# Print the numbers between num1 and num2
