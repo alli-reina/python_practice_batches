79b6d80b00352c6f25161a054288accfb3acc791
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 23:18:35 +0800

    Updated: integer_quotient_of_two_numbers.py
    Calculate the quotient and print the result

--- integer_quotient_of_two_numbers.py
+++ integer_quotient_of_two_numbers.py
@@ -6,3 +6,5 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
 # Calculate the quotient and print the result
+quotient = num1 // num2
+print("Quotient:", quotient)
