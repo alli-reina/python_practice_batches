8c0eb87790e05a2d23540e849b52f4af80ca1159
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 23:41:45 +0800

    Updated: count_even_numbers.py
    Initialize the even number count to 0

--- count_even_numbers.py
+++ count_even_numbers.py
@@ -1,6 +1,9 @@
 # Program: Count the Even Numbers
 # This program asks the user to input 10 numbers and prints how many of them are even numbers
+
 # Initialize the even number count to 0
+even_count = 0
+
 # Ask the user to input 10 numbers
 # Check if the number is even
 # Print the total number of even numbers
