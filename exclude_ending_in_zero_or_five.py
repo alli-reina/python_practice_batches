7fc60b721a43bf4eee9cdaa641223db62dbe9bb1
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-12 00:53:36 +0800

    Update exclude_ending_in_zero_or_five.py
    Fixed the code

--- exclude_ending_in_zero_or_five.py
+++ exclude_ending_in_zero_or_five.py
@@ -1,7 +1,7 @@
 # Program: Print Numbers from 0 to 100, excluding those ending in 0 or 5
 # This program prints all the numbers from 0 to 100, but skips the ones ending in 0 or 5
 
-# Loop through the numbers from 0 to 100
+# Loop through the numbers from 0 to 100 exclude numbers ending with 5 or 0
 for i in range(101):
     # Check if the number ends in 0 or 5
     if i % 10 == 0 or i % 10 == 5:
