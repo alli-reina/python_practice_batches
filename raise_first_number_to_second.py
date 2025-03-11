268a25880bff86baf9a51b1d9d6e1a962b90c050
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 18:58:09 +0800

    Added  raise_first_number_to_second.py
    pseudocode Program: Raise the First Number to the Power of the Second Number

---
+++ raise_first_number_to_second.py
@@ -0,0 +1,6 @@
+# Program: Raise the First Number to the Power of the Second Number
+# This program asks the user for two numbers and prints the result when the first number is raised to the power of the second number.
+# Ask the user for two numbers
+# Calculate num1 raised to the power of num2
+# Print the result
+
