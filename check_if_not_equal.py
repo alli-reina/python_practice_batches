159df210619066261a1d2d3a09bc0c5872f66b70
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 23:02:39 +0800

    Updated: check_if_not_equal.py
    Check if the numbers are not equal and print 'Not Equal' if true

--- check_if_not_equal.py
+++ check_if_not_equal.py
@@ -1,9 +1,10 @@
 # Program: Check if the Numbers are Not Equal
-# This program asks the user to input two numbers and prints "Not Equal" 
-# if the numbers are different
+# This program asks the user to input two numbers and prints "Not Equal" if the numbers are different
 
 # Ask the user for two numbers
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
 # Check if the numbers are not equal and print "Not Equal" if true
+if num1 != num2:
+    print("Not Equal")
