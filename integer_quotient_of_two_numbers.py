de7ba0dec9bee41cc31d23f7ad09e23e26ed1664
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 23:17:23 +0800

    Updated: integer_quotient_of_two_numbers.py
    Ask the user for two numbers

--- integer_quotient_of_two_numbers.py
+++ integer_quotient_of_two_numbers.py
@@ -1,4 +1,8 @@
 # Program: Print the Quotient of Two Numbers
 # This program asks the user to input two numbers and prints the quotient (integer division) of the two numbers
+
 # Ask the user for two numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
 # Calculate the quotient and print the result
