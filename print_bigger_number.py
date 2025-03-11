61c2c7aa0c04092a45b4e00f5dce976361d166a3
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 17:08:58 +0800

    Updated print_bigger_number.py
    Ask the user for numbers

--- print_bigger_number.py
+++ print_bigger_number.py
@@ -1,6 +1,6 @@
-Ask the user for the first number (num1).
-Ask the user for the second number (num2).
-Compare the two numbers
-If num1 is greater than num2, print num1 (the bigger number).
-If num2 is greater than num1, print num2 (the bigger number).
-If both are equal, print "Equal".
+#Ask the user for the numbers
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+
+#Compare the two numbers
+#If both are equal, print "Equal".
