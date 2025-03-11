b73c3fccee274addf5a1fe5fd67979686ce6bd6d
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-12 00:52:45 +0800

    Update odd_numbers_while_loop.py
    Fixed the code

--- odd_numbers_while_loop.py
+++ odd_numbers_while_loop.py
@@ -1,5 +1,7 @@
 # Program: Print Odd Numbers from 0 to 100 using While Loop
 # This program prints all the odd numbers from 0 to 100 using a while loop.
 
-for i in range(1, 101, 2):  # Start from 1 and step by 2 to get odd numbers *Easier*
+i = 1  # Start with the first odd number
+while i <= 100:
     print(i, end=" ")
+    i += 2  # Increment by 2 to get the next odd number)
