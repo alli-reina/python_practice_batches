e09b64d41ace2577af9606dfac431d8b2bad6772
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 17:51:26 +0800

    Added sum_of_two_numbers.py
    Pseudocode  Program: Print the Sum of Two Numbers

---
+++ sum_of_two_numbers.py
@@ -0,0 +1,6 @@
+# Program: Print the Sum of Two Numbers
+# This program asks the user to input two numbers and prints their sum.
+# Ask the user for two numbers
+# Calculate the sum of the two numbers
+# Print the result
+
