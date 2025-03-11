0076eb797d977aa9d33707be1b567394470977ad
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 17:16:59 +0800

    Updated print_bigger_number.py
    Compare and Print Bigger number

--- print_bigger_number.py
+++ print_bigger_number.py
@@ -3,4 +3,10 @@ num1 = int(input("Enter 1st number: "))
 num2 = int(input("Enter 2nd number: "))
 
 #Compare the two numbers
-#If both are equal, print "Equal".
+#If both are equal, print "Equal"
+if num1 > num2:
+    print(num1)
+elif num1 < num2:
+    print(num2)
+else:
+    print("Equal")
