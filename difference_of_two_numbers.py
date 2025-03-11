5dcb33bacc06d4b3631e7ba165359078f06e7cc5
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-11 23:09:25 +0800

    Updated: difference_of_two_numbers.py
    Ask the user for two numbers

--- difference_of_two_numbers.py
+++ difference_of_two_numbers.py
@@ -1,4 +1,8 @@
 # Program: Print the Difference of Two Numbers
 # This program asks the user to input two numbers and prints the difference between them
+
 # Ask the user for two numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
 # Calculate the difference and print the result
