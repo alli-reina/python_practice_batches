921c083d027ee28dd1539ce1c1917ba5f239c25a
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-21 16:36:42 +0800

    Update check_equal_numbers.py
    There are many unnecessary comments.

--- basic_arithmetic_operations_and_comparisons/check_equal_numbers.py
+++ basic_arithmetic_operations_and_comparisons/check_equal_numbers.py
@@ -1,11 +1,8 @@
 # Program Name: Check if Two Numbers Are Equal
 # This program asks the user to input two numbers and checks if they are equal
-# If the numbers are equal, it prints "Equal", otherwise it prints "Not Equal"
 
-# Ask the user for two numbers
 num1 = int(input("Enter 1st number: "))
 num2 = int(input("Enter 2nd number: "))
 
-# Compare the two numbers and print the result
 if num1 == num2:
     print("Equal")
