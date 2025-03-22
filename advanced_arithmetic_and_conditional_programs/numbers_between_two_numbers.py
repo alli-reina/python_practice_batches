5d4030164c114773b605a9cd445b846144c4c670
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-22 16:21:11 +0800

    Update numbers_between_two_numbers.py
    too much comments

--- advanced_arithmetic_and_conditional_programs/numbers_between_two_numbers.py
+++ advanced_arithmetic_and_conditional_programs/numbers_between_two_numbers.py
@@ -1,11 +1,9 @@
 # Program: Print All Numbers Between Two Numbers
 # This program asks the user to input two numbers and prints all the numbers between them (exclusive)
 
-# Ask the user for two numbers
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
-# Print the numbers between num1 and num2
 if num1 < num2:
     for i in range(num1 + 1, num2):
         print(i, end=" ")
